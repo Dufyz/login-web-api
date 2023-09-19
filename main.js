56780ea68595da9d543da3b363d4544dd92104f0
Guilherme I. Schmidt T. <80853300+Dufyz@users.noreply.github.com>
2023-09-18 22:29:48 -0300

    Crud teste com html web

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // http://localgost:3000/create_user
 
+const path = require('path');
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
@@ -40,7 +41,24 @@ app.post('/create_user', async (req, res) => {
     }
 });
 
+app.get('/users', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const [users] = await connection.execute('SELECT pkUser, login, email, created_at FROM Usuarios');
+        connection.end();
+
+        res.status(200).send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Internal server error' });
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
