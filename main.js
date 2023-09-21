d215686b81d5a80006b5f919f3d27986c91eeccb
Dufyz <guilhermeiagothomaz@hotmail.com>
2023-09-21 20:42:22 -0300

    autenticacao funcionando

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ const dbConfig = {
 
 // Middleware para analisar corpos de solicitação JSON
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static(__dirname + '/web'));
 
 app.post('/create_user', async (req, res) => {
@@ -55,11 +58,42 @@ app.get('/users', async (req, res) => {
     }
 });
 
-
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'web/public/index.html'));
+});
+
+app.post('/authenticate_user', async (req, res) => {
+    try {
+        const {login, password } = req.body;
+
+        const connection = await mysql.createConnection(dbConfig);
+
+        // Consulta o banco de dados para obter a senha hash para o user_name fornecido
+        const [rows] = await connection.execute('SELECT pwdHash FROM Usuarios WHERE user_name = ?', [login]);
+
+        if (rows.length > 0) {
+            const pwdHashFromDB = rows[0].pwdHash;
+
+            // Usa bcrypt para verificar se a senha fornecida corresponde à senha hash do banco de dados
+            const passwordIsValid = bcrypt.compareSync(password, pwdHashFromDB);
+
+            if (passwordIsValid) {
+                res.status(200).send("Usuário autenticado com sucesso!");
+            } else {
+                res.status(401).send("Senha incorreta!");
+            }
+        } else {
+            res.status(404).send("Usuário não encontrado!");
+        }
+
+        connection.end();
+    } catch (error) {
+        console.error("Erro detalhado:", error);  // Log do erro detalhado
+        res.status(500).send("Erro ao autenticar o usuário!");
+    }
+    
 });
--- web/public/assets/js/script.js
+++ web/public/assets/js/script.js
@@ -1,15 +1,15 @@
 function toggleSections() {
     const loginForm = document.getElementById('login-form');
     const registerForm = document.getElementById('register-form');
-    
+
     const showRegisterLink = document.getElementById('show-register-link');
     const showLoginLink = document.getElementById('show-login-link');
-    
+
     showRegisterLink.addEventListener('click', () => {
         loginForm.style.display = 'none';
         registerForm.style.display = 'block';
     });
-    
+
     showLoginLink.addEventListener('click', () => {
         loginForm.style.display = 'block';
         registerForm.style.display = 'none';
@@ -18,3 +18,48 @@ function toggleSections() {
 
 // Chame a função para habilitar o comportamento de alternância das seções
 toggleSections();
+
+// Função para carregar os usuários
+function loadUsers() {
+    fetch('/users')
+        .then(response => response.json())
+        .then(users => {
+            const table = document.getElementById('usersTable');
+            users.forEach(user => {
+                const row = table.insertRow();
+                row.insertCell(0).textContent = user.pkUser;
+                row.insertCell(1).textContent = user.login;
+                row.insertCell(2).textContent = user.email;
+                row.insertCell(3).textContent = user.created_at;
+            });
+        });
+}
+
+// Função para lidar com o envio do formulário
+function submitForm(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+        login: formData.get('user'),
+        email: formData.get('email'),
+        password: formData.get('senha')
+    };
+    fetch('/create_user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.message) {
+                alert(result.message);
+                // Recarregar os usuários após o registro
+                loadUsers();
+            }
+        });
+}
+
+// Carregar os usuários quando a página for carregada
+window.onload = loadUsers;
