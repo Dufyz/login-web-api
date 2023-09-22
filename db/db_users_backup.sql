f30c68cf5ecbc1212c7b2985ae8dc8bbd4a4b8db
Dufyz <guilhermeiagothomaz@hotmail.com>
2023-09-21 21:59:21 -0300

    alterando db; editando profile page

--- db/db_users_backup.sql
+++ db/db_users_backup.sql
@@ -12,26 +12,14 @@ CREATE TABLE Usuarios(
     modify_at DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Endereco(
-    pkEndereco INT PRIMARY KEY AUTO_INCREMENT,
-    estado VARCHAR(2) NOT NULL,
-    cidade VARCHAR(255) NOT NULL,
-    rua VARCHAR(255) NOT NULL,
-    cep VARCHAR(8) NOT NULL,
-    fkUser INT NOT NULL UNIQUE
-);
+-- CREATE TABLE Endereco(
+--     pkEndereco INT PRIMARY KEY AUTO_INCREMENT,
+--     estado VARCHAR(2) NOT NULL,
+--     cidade VARCHAR(255) NOT NULL,
+--     rua VARCHAR(255) NOT NULL,
+--     cep VARCHAR(8) NOT NULL,
+--     fkUser INT NOT NULL UNIQUE
+-- );
 
-ALTER TABLE Endereco 
-ADD CONSTRAINT fk_Usuarios_Endereco FOREIGN KEY(fkUser) REFERENCES Usuarios(pkUser);
-
-CREATE TABLE Post(
-    pkPost INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    body VARCHAR(255) NOT NULL,
-    likes INT,
-    fkUser INT NOT NULL
-);
-
-ALTER TABLE Post
-ADD CONSTRAINT fk_Usuarios_Post FOREIGN KEY(fkUser) REFERENCES Usuarios(pkUser);
-
+-- ALTER TABLE Endereco 
+-- ADD CONSTRAINT fk_Usuarios_Endereco FOREIGN KEY(fkUser) REFERENCES Usuarios(pkUser);
